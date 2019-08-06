d6f8a2de253375c9015b72cc6000267fc2852261
Domenico Flauto <domi989@gmail.com>
2019-08-06 23:05:39 +0100

    adds custom styles - barebone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.sass';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="name">
+        flauto
+        <span>.design</span>
+      </h1>
+      <div>
+        <span className="subtext">
+          coming soon
+        </span>
+      </div>
     </div>
   );
 }
